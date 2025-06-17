e0362239e7b44c90dc66d7775a156b94f9bd0e2d
Admin <Admin@DESKTOP-KGH4IEA>
2025-06-17 16:11:44 +0530

    commit

--- src/main/java/com/GunaMarket/POM/ServicePage.java
+++ src/main/java/com/GunaMarket/POM/ServicePage.java
@@ -1,55 +1,54 @@
 package com.GunaMarket.POM;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ServicePage {
 
+    private WebDriver driver;
+    private WebDriverWait wait;
 
-	    private WebDriver driver;
+    @FindBy(xpath = "//a[text()='Explore Our Services']")
+    private WebElement servicelink; 
 
-	    @FindBy(xpath = "//a[text()='Explore Our Services']")
-	    private WebElement servicelink; 
+    @FindBy(id = "videoAdvertisement")
+    private WebElement videoAdOption;
 
-	    @FindBy(id = "videoAdvertisement")
-	    private WebElement videoAdOption;
+    @FindBy(xpath = "//input[@value='Activate']")
+    private WebElement activateButton;
 
-	    @FindBy(xpath = "//input[@value='Activate']")
-	    private WebElement activateButton;
+    public ServicePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        PageFactory.initElements(driver, this);
+    }
 
-	    public ServicePage(WebDriver driver) {
-	        this.driver = driver;
-	        PageFactory.initElements(driver, this);
-	    }
+    public void exploreService() {
+        wait.until(ExpectedConditions.visibilityOf(servicelink));
+        servicelink.click();
+    }
 
-	    public void exploreService() {
-	        servicelink.click();
-	    }
+    public void clickVideoAdvertisement() {
+        wait.until(ExpectedConditions.elementToBeClickable(videoAdOption));
+        videoAdOption.click();
+    }
 
-	    public void clickVideoAdvertisement() {
-	        videoAdOption.click();
-	    }
+    public void clickActivateButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(activateButton));
+        activateButton.click();
+    }
 
-	    public void clickActivateButton() {
-	        activateButton.click();
-	    }
-
-	    // Combined flow
-	    public void servicing() {
-	        exploreService();
-	        clickVideoAdvertisement();
-	        clickActivateButton();
-	    }
-	}
-	
-
-
-
-
-
-
-
-
-
+    // Combined flow
+    public void servicing() {
+        exploreService();
+        clickVideoAdvertisement();
+        clickActivateButton();
+    }
+}
